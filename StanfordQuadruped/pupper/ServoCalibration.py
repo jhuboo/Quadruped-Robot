f0a87480796310fd4bec380ce5b9d1aba5ca720a
Anvesh <rgjhuboo@gmail.com>
2021-11-17 14:18:34 -0500

    Added StandfordQuadruped/ based on the work of Nathan Kau

---
+++ StanfordQuadruped/pupper/ServoCalibration.py
@@ -0,0 +1,11 @@
+# WARNING: This file is machine generated. Edit at your own risk.
+
+import numpy as np
+
+
+MICROS_PER_RAD = 11.111 * 180.0 / np.pi
+NEUTRAL_ANGLE_DEGREES = np.array(
+[[  5.,  2., -5.,  3.],
+ [ 37., 45., 40., 55.],
+ [-28.,-21.,-36.,-39.]]
+)
